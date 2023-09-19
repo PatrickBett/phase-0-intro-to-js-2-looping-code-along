382b11e70fc137ff1003ce3b45a11052c83b9113
PatrickBett <patrickbett018@gmail.com>
2023-09-19 22:37:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const name = ['Guadalupe', 'Ollie', 'Aki'];
+const eventName ="surprise";
+
+function writeCards(name,eventName) {
+  for (let i = 0; i < name.length; i++) {
+    console.log(`Thank you,${name[i]} , for the wonderful ${eventName} gift!`);
+    debugger;
+  }
+
+  return name;
+}
+
+writeCards();
+
+function countDown(){
+    let num=0;
+    while(num<11){
+        console.log(num++);
+
+    }
+}
